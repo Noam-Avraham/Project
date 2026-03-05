9dc4f98722a41d3bff45741afa32326c9401bea2
noam avraham <noamavraham1@mail.tau.ac.il>
2026-03-05 18:33:57 +0200

    read points from imported symnmf .py

--- analysis.py
+++ analysis.py
@@ -11,7 +11,7 @@ def main():
     k = int(sys.argv[1])
     file_name = sys.argv[2]
     #using the read_points function from symnmf to read the data points
-    points = read_points(file_name)
+    points = symnmf.read_points(file_name)
 
     H = symnmf.main_function(points, k,"symnmf")
     nmf_labels = np.argmax(H, axis=1)
@@ -30,32 +30,6 @@ def main():
     # הדפסה בפורמט הנדרש
     print(f"nmf: {nmf_score:.4f}")
     print(f"kmeans: {kmeans_score:.4f}")
-def read_points(file_name):
-    # pts is list of list of floats
-    points = []
-    try:
-        with open(file_name, 'r') as f:
-            lines = f.read().strip().splitlines()
-    except Exception:
-        print("An Error Has Occurred")
-        sys.exit(1)
-
-    for line in lines:
-        if not line.strip():
-            continue
-        coards_string=line.split(',')
-        current_point=[]
-        
-        for coard in coards_string:
-            try:
-                coard_float = float(coard)
-            except ValueError:
-                print("An Error Has Occurred")
-                sys.exit(1)
-            coard_float=float(coard)
-            current_point.append(coard_float)
-        points.append(current_point)
-    return points
 
 if __name__ == "__main__":
     main()
